5e7a6ddb2c246dbe3892c3b4a334da9f4eb51ed4
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-10-16 14:26:56 +0300

    First commit

---
+++ Lab_5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Lab 5_1
+class Program
+{
+    static void Main(string[] args)
+    {
+        double s, t;
+        Console.Write("a = "); s = double.Parse(Console.ReadLine());
+        Console.Write("b = "); t = double.Parse(Console.ReadLine());
+        double c = (F(1,t*t,s) + F(t,s*s,1))
+                   /(1 + F(1,t*s,1)*F(1, t * s, 1));
+        Console.WriteLine("c = " + c);
+    }
+    static double F ( double a,  double b,  double c)
+    {
+        return Math.Sin(a*b*c) * Math.Sin(a * b * c)
+               /(a*a + b*b + c*c);
+    }
+}
